bcec89b9dbffae8c29006fc83c6b2d5b5c230d81
Jayram Sharma <147648366+jayram0402@users.noreply.github.com>
2024-04-23 13:11:15 +0530

    BankAppApplication.java

---
+++ src/main/java/io/bankapp/BankAppApplication.java
@@ -0,0 +1,19 @@
+
+package io.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Jayram Sharma
+ *
+ */
+
+@SpringBootApplication
+public class BankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppApplication.class, args);
+	}
+
+}
